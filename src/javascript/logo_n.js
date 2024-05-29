a66f236fd5e6d22dbf419f6012a345d81537d3cd
sofiadalsgaard <soda0001@stud.kea.dk>
2024-05-29 16:22:47 +0200

    logo link

--- src/javascript/logo_n.js
+++ src/javascript/logo_n.js
@@ -2,12 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const logoLink = document.getElementById("logo-link");
   const currentPage = window.location.pathname;
 
-  if (currentPage.includes("/menu")) {
+  if (currentPage.includes("/menu_norrebro") || currentPage.includes("/about_norrebro")) {
     logoLink.href = "/forside_norrebro";
-  } else if (currentPage.includes("/about")) {
-    logoLink.href = "/forside_norrebro";
-  } else if (currentPage.includes("/norrebro")) {
-    logoLink.href = "/forside_norrebro";
+  } else if (currentPage === "/forside_norrebro") {
+    logoLink.href = "/";
   } else {
     logoLink.href = "/";
   }
--- src/javascript/logo_v.js
+++ src/javascript/logo_v.js
@@ -2,12 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const logoLink = document.getElementById("logo-link");
   const currentPage = window.location.pathname;
 
-  if (currentPage.includes("/menu")) {
+  if (currentPage.includes("/menu_vesterbro") || currentPage.includes("/about_vesterbro")) {
     logoLink.href = "/forside_vesterbro";
-  } else if (currentPage.includes("/about")) {
-    logoLink.href = "/forside_vesterbro";
-  } else if (currentPage.includes("/vesterbro")) {
-    logoLink.href = "/forside_vesterbro";
+  } else if (currentPage === "/forside_vesterbro") {
+    logoLink.href = "/";
   } else {
     logoLink.href = "/";
   }
