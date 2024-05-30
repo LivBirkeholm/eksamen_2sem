82606e22f6c10cf57e4024eefa5a79ffee22f67b
sofiadalsgaard <soda0001@stud.kea.dk>
2024-05-30 09:54:29 +0200

    logolink

--- src/javascript/logo_v.js
+++ src/javascript/logo_v.js
@@ -2,11 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const logoLink = document.getElementById("logo-link");
   const currentPage = window.location.pathname;
 
+  console.log("skriv currentPage", currentPage);
+
   if (currentPage.includes("/menu_vesterbro") || currentPage.includes("/about_vesterbro")) {
     logoLink.href = "/forside_vesterbro";
   } else if (currentPage === "/forside_vesterbro") {
-    logoLink.href = "index.html";
+    logoLink.href = "../index.html";
   } else {
-    logoLink.href = "index.html";
+    logoLink.href = "../index.html";
   }
 });
