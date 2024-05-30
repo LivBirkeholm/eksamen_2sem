e8318e6f0d265a3308dcafcc37358f8fae6d76b5
sofiadalsgaard <soda0001@stud.kea.dk>
2024-05-30 09:58:49 +0200

    link menu

--- src/javascript/logo_v.js
+++ src/javascript/logo_v.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const logoLink = document.getElementById("logo-link");
   const currentPage = window.location.pathname;
 
-  console.log("skriv currentPage", currentPage);
+  console.log("skriv currentPage:", currentPage);
 
   if (currentPage.includes("/menu_vesterbro") || currentPage.includes("/about_vesterbro")) {
     logoLink.href = "/forside_vesterbro";
